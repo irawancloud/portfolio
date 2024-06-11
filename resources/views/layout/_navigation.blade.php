f233e4780c86859b5cadbdc9fba4ed38e85c9263
irawanbellamy <irawanbellamy@gmail.com>
2024-06-11 14:13:10 +0700

    rubah navigasi

--- resources/views/layout/_navigation.blade.php
+++ resources/views/layout/_navigation.blade.php
@@ -13,24 +13,24 @@
                 <h2>About</h2>
             </a>
         </li>
-        <li class="icon-box {{ $url == 'portfolio'? $aktif : '' }}">
+        <!-- <li class="icon-box {{ $url == 'portfolio'? $aktif : '' }}">
             <i class="fa fa-briefcase"></i>
             <a href="{{ route('portfolio') }}">
                 <h2>Portfolio</h2>
             </a>
-        </li>
+        </li> -->
         <li class="icon-box {{ $url == 'contact'? $aktif : '' }}">
             <i class="fa fa-envelope-open"></i>
             <a href="{{ route('contact') }}">
                 <h2>Contact</h2>
             </a>
         </li>
-        <li class="icon-box {{ $url == 'blog'? $aktif : '' }}">
+        <!-- <li class="icon-box {{ $url == 'blog'? $aktif : '' }}">
             <i class="fa fa-comments"></i>
             <a href="{{ route('blog') }}">
                 <h2>Blog</h2>
             </a>
-        </li>
+        </li> -->
     </ul>
     <!-- Fixed Navigation Ends -->
     <!-- Mobile Menu Starts -->
@@ -43,9 +43,9 @@
             <ul class="list-unstyled" id="menu">
                 <li class="active"><a href="{{ route('index') }}"><i class="fa fa-home"></i><span>Home</span></a></li>
                 <li><a href="{{ route('about') }}"><i class="fa fa-user"></i><span>About</span></a></li>
-                <li><a href="{{ route('portfolio') }}"><i class="fa fa-folder-open"></i><span>Portfolio</span></a></li>
+                <!-- <li><a href="{{ route('portfolio') }}"><i class="fa fa-folder-open"></i><span>Portfolio</span></a></li> -->
                 <li><a href="{{ route('contact') }}"><i class="fa fa-envelope-open"></i><span>Contact</span></a></li>
-                <li><a href="{{ route('blog') }}"><i class="fa fa-comments"></i><span>Blog</span></a></li>
+                <!-- <li><a href="{{ route('blog') }}"><i class="fa fa-comments"></i><span>Blog</span></a></li> -->
             </ul>
         </div>
     </nav>
